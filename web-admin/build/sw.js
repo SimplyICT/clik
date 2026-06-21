e2ddd37cdf738427ea7652621eb8aebdd76a0fcf
SimplyICT <simplyict@users.noreply.github.com>
2026-06-21 03:07:45 +0000

    Remove service worker from admin app - fixing SPA navigation

--- web-admin/build/sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE = 'simplyclik-admin-v2';
-const ASSETS = ['/', '/manifest.json', '/icon-192.svg'];
-self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener('activate', (e) => { e.waitUntil(clients.claim()); });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match('/'))
-  );
-});
